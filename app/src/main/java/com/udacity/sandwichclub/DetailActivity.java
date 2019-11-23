acfebef7e539bd43ac121953cbc48eb721fc1f2e
Shuayb Khan <shuaybk@gmail.com>
2019-11-22 23:04:42 -0500

    Added JSON parser and added basic layout to display details data.

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -4,16 +4,25 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
 
+import org.w3c.dom.Text;
+
 public class DetailActivity extends AppCompatActivity {
 
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
+
+    TextView mAka_tv;
+    TextView mIngredients_tv;
+    TextView mOrigin_tv;
+    TextView mDescription_tv;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +30,11 @@ public class DetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail);
 
         ImageView ingredientsIv = findViewById(R.id.image_iv);
+
+        mAka_tv = (TextView) findViewById(R.id.aka_tv);
+        mIngredients_tv = (TextView) findViewById(R.id.ingredients_tv);
+        mOrigin_tv = (TextView) findViewById(R.id.origin_tv);
+        mDescription_tv = (TextView) findViewById(R.id.description_tv);
 
         Intent intent = getIntent();
         if (intent == null) {
@@ -36,6 +50,7 @@ public class DetailActivity extends AppCompatActivity {
 
         String[] sandwiches = getResources().getStringArray(R.array.sandwich_details);
         String json = sandwiches[position];
+        System.out.println(json);
         Sandwich sandwich = JsonUtils.parseSandwichJson(json);
         if (sandwich == null) {
             // Sandwich data unavailable
@@ -49,6 +64,10 @@ public class DetailActivity extends AppCompatActivity {
                 .into(ingredientsIv);
 
         setTitle(sandwich.getMainName());
+        mAka_tv.setText(sandwich.getAlsoKnownAs().toString());
+        mIngredients_tv.setText(sandwich.getIngredients().toString());
+        mOrigin_tv.setText(sandwich.getPlaceOfOrigin());
+        mDescription_tv.setText(sandwich.getDescription());
     }
 
     private void closeOnError() {
--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -2,9 +2,59 @@ package com.udacity.sandwichclub.utils;
 
 import com.udacity.sandwichclub.model.Sandwich;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class JsonUtils {
 
+
     public static Sandwich parseSandwichJson(String json) {
-        return null;
+        Sandwich result;
+        String name;
+        List<String> alsoKnownAs = new ArrayList<>();
+        String placeOfOrigin;
+        String description;
+        String image;
+        List<String> ingredients = new ArrayList<>();
+
+        try {
+            JSONObject jsonSandwich = new JSONObject(json);
+            name = jsonSandwich.getJSONObject("name").getString("mainName");
+            placeOfOrigin = jsonSandwich.getString("placeOfOrigin");
+            description = jsonSandwich.getString("description");
+            image = jsonSandwich.getString("image");
+
+            JSONArray nameArr = jsonSandwich.getJSONObject("name").getJSONArray("alsoKnownAs");
+            for (int i = 0; i < nameArr.length(); i++) {
+                alsoKnownAs.add(nameArr.getString(i));
+            }
+
+            JSONArray ingArr = jsonSandwich.getJSONArray("ingredients");
+            for (int i = 0; i < ingArr.length(); i++) {
+                ingredients.add(ingArr.getString(i));
+            }
+
+            result = new Sandwich(name, alsoKnownAs, placeOfOrigin, description, image, ingredients);
+
+            return result;
+        } catch (Exception e) {
+            //To be polished
+            return null;
+        }
     }
 }
+
+
+//  {
+//    "name":{
+//      "mainName":"Bosna",
+//      "alsoKnownAs":["Bosner"]
+//    },
+//    "placeOfOrigin":"Austria",
+//    "description":"Bosna is a spicy Austrian fast food dish, said to have originated in either Salzburg or Linz. It is now popular all over western Austria and southern Bavaria.",
+//    "image":"https://upload.wikimedia.org/wikipedia/commons/c/ca/Bosna_mit_2_Bratw%C3%BCrsten.jpg",
+//    "ingredients":["White bread","Bratwurst","Onions","Tomato ketchup","Mustard","Curry powder"]
+//  }
